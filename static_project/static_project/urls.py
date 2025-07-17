e315c2e2bf19fb457a6de9b8caa4c3773bcf8dd2
mingtiancai <2427373908@qq.com>
2025-07-17 14:34:29 +0000

    add show static image

--- static_project/static_project/urls.py
+++ static_project/static_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myproject project.
+URL configuration for static_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,16 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
-
 from django.contrib import admin
 from django.urls import path
-from myapp.views import home
+from app.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
