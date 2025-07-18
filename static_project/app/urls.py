9dd81c4a91e89e644816ff5ebf2f4df5bd462aae
mingtiancai <2427373908@qq.com>
2025-07-18 10:30:58 +0000

    use url module name and parameter

--- static_project/app/urls.py
+++ static_project/app/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns=[
     path("",views.home),
     path("hello/",views.hello),
+    # path('<year>/<int:month>/<slug:day>',views.data),
+    # re_path('(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2}).html',views.date),
+    re_path('(?P<year>[0-9]{4}).html',views.year,name='my_year'),
+    re_path('dict/(?P<year>[0-9]{4}).htm',views.my_dict,{'month':'05'},name='my_dict')
 ]
--- static_project/app/views.py
+++ static_project/app/views.py
@@ -6,3 +6,12 @@ def home(request):
 
 def hello(request):
     return HttpResponse("hello world")
+
+def date(request,year,month,day):
+    return HttpResponse(str(year)+'/'+str(month)+'/'+str(day))
+
+def year(request,year):
+    return render(request,"myyear.html")
+
+def my_dict(request,year,month):
+    return render(request,"myyear_dict.html",{'month':month})
