ad42f30de67f7d051b541807bc27b592e0e7c95d
mingtiancai <2427373908@qq.com>
2025-07-18 13:20:56 +0000

    add dataset interface

--- static_project/app/urls.py
+++ static_project/app/urls.py
@@ -7,5 +7,8 @@ urlpatterns=[
     # path('<year>/<int:month>/<slug:day>',views.data),
     # re_path('(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2}).html',views.date),
     re_path('(?P<year>[0-9]{4}).html',views.year,name='my_year'),
-    re_path('dict/(?P<year>[0-9]{4}).htm',views.my_dict,{'month':'05'},name='my_dict')
+    re_path('dict/(?P<year>[0-9]{4}).htm',views.my_dict,{'month':'05'},name='my_dict'),
+    path("download.html",views.download),
+    path("google/",views.google),
+    path("database/",views.get_data)
 ]
--- static_project/app/views.py
+++ static_project/app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import csv
+from . import models
 
 def home(request):
     return render(request, 'home.html')
@@ -15,3 +17,20 @@ def year(request,year):
 
 def my_dict(request,year,month):
     return render(request,"myyear_dict.html",{'month':month})
+
+def download(request):
+    response=HttpResponse(content_type="text/csv")
+    response['Content-Disposition']='attachment;filename="somefilename.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['First row','A','B','C'])
+    return response
+
+def google(request):
+    return render(request,"Google.html")
+
+def get_data(request):
+    print("----------------------")
+    items = models.Product.objects.all()
+    print(items)
+    # return render(request,"myyear.html")
+    return render(request, 'database.html', {'items': items})
